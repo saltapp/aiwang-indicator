e1f1b495511bc42df589cc7c6ae2413001b7c997
saltapp <815625981@qq.com>
2025-06-18 08:12:36 +0800

    Implement subtraction and division operations for BaseDatedSeries class, enhancing arithmetic functionality

--- aiwang_indicator/model/series.py
+++ aiwang_indicator/model/series.py
@@ -21,11 +21,47 @@ class BaseDatedSeries:
         
         return self.__class__(result.values, all_dates)
     
+    def __sub__(self, other):
+        if isinstance(other, (int, float)):
+            return self.__class__(self.data + other, self.data.index)
+        
+        # 找出所有时间点的并集
+        all_dates = self.data.index.union(other.data.index)
+        
+        # 用最后一个有效值向前填充
+        series1 = self.data.reindex(all_dates, method='ffill')
+        series2 = other.data.reindex(all_dates, method='ffill')
+        
+        # 计算结果
+        result = series1 - series2
+        
+        return self.__class__(result.values, all_dates)
+    
     def __mul__(self, other):
-        return self.__class__(self.data * other, self.data.index)
+        if isinstance(other, (int, float)):
+            return self.__class__(self.data * other, self.data.index)
+        
+        all_dates = self.data.index.union(other.data.index)
+        series1 = self.data.reindex(all_dates, method='ffill')
+        series2 = other.data.reindex(all_dates, method='ffill')
+        result = series1 * series2
+        return self.__class__(result.values, all_dates)
+
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            return self.__class__(self.data / other, self.data.index)
+        
+        all_dates = self.data.index.union(other.data.index)
+        series1 = self.data.reindex(all_dates, method='ffill')
+        series2 = other.data.reindex(all_dates, method='ffill')
+        result = series1 / series2
+        return self.__class__(result.values, all_dates)
     
     def __rmul__(self, other):
         return self.__mul__(other)
+    
+    def __rtruediv__(self, other):
+        return self.__truediv__(other)
     
     def __getitem__(self, key):
         return self.data[key]
